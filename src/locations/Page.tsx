478e07b8e8ca335665adc5d41b5ab7d6b5c10262
Anil <anshetty1497@gmail.com>
2024-08-08 13:57:37 +0530

    bootstrapped with create-contentful-app

---
+++ src/locations/Page.tsx
@@ -0,0 +1,10 @@
+import { PageAppSDK } from '@contentful/app-sdk';
+import { Paragraph } from '@contentful/f36-components';
+import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+
+const Page = () => {
+  const sdk = useSDK<PageAppSDK>();
+  return <Paragraph>Hello Page Component (AppId: {sdk.ids.app})</Paragraph>;
+};
+
+export default Page;
